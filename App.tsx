bb99e712c931a399288a34500568b1391779a666
diegoalvescf <dalvesdocs@gmail.com>
2023-02-16 22:40:30 -0300

    add status bar

--- App.tsx
+++ App.tsx
@@ -1,7 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { StatusBar, Text, TouchableOpacity, View } from 'react-native';
 import { Home } from './src/presentation/jsx/screens/home';
 
 export default function App() {
-  return <Home />;
+  return (
+    <>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor="transparent"
+        translucent
+      />
+      <Home />
+    </>
+  );
 }
